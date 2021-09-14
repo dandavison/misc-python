70e345b237d5c4ead084aeb26bb777235e0343f3
Dan Davison <dandavison7@gmail.com>
2021-09-14 10:24:47 -0400

    trie

--- trie.py
+++ trie.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-TERMINATOR = '$'
+TERMINATOR = "$"
 
 
-class Trie:
-
+class Trie_1:
     def __init__(self):
         self.root = {}
 
@@ -30,11 +29,31 @@ class Trie:
         return node
 
 
-if __name__ == '__main__':
+# https://leetcode.com/problems/word-search-ii/discuss/59804/27-lines-uses-complex-numbers/580993
+from collections import defaultdict
+from functools import reduce
+
+Trie = lambda: defaultdict(Trie)
+
+trie = Trie()
+words = ["dig", "dog", "dag", "dags"]
+for word in words:
+    reduce(dict.__getitem__, word, trie)["$"] = True
+print(trie)
+exit()
+
+
+class Trie_2:
+    def __init__(self):
+        _Trie = lambda: defaultdict(_Trie)
+        self.trie = _Trie
+
+
+if __name__ == "__main__":
     t = Trie()
-    t.add_word('fish')
-    t.add_word('trombone')
-    t.add_word('rex')
-    assert t.is_word('rex')
-    assert t.is_prefix('trom')
-    assert not t.is_word('rexs')
+    t.add_word("fish")
+    t.add_word("trombone")
+    t.add_word("rex")
+    assert t.is_word("rex")
+    assert t.is_prefix("trom")
+    assert not t.is_word("rexs")
