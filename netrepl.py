83652fbe55eb544a2bfd5225a0dfefb14340eb38
Dan Davison <dandavison7@gmail.com>
2024-09-06 22:25:59 -0400

    shell version of netrepl

--- netrepl.py
+++ netrepl.py
@@ -1,13 +1,26 @@
+#!/usr/bin/env python
+"""
+1. Run ngrok on your local machine, e.g. `ngrok tcp 7777` and note the ngrok IP and port
+
+2. Add a line like this to a GitHub Actions YAML job definition:
+   ```
+   ./oss-cicd/internal/github/netrepl <ngrok-ip> <ngrok-port>
+   ```
+
+3. Run `nc -l 7777` on your local machine, and then run the GitHub workflow
+"""
+
 import socket
+import subprocess
 import traceback
 from typing import Callable
 
 
-def netrepl(address: str, port: int, globals: dict, locals: dict):
-    from repl import repl
-
+def netrepl(address: str, port: int):
     sock = socket.socket()
+    print(f"connecting to {address}:{port}")
     sock.connect((address, port))
+    print("starting REPL")
 
     def read():
         return sock.recv(0xFFFF).decode("utf-8")
@@ -15,16 +28,19 @@ def netrepl(address: str, port: int, globals: dict, locals: dict):
     def write(s: str):
         sock.send(s.encode("utf-8"))
 
-    repl(read, write, globals, locals)
+    repl(read, write)
 
 
-def repl(
-    read: Callable[[], str], write: Callable[[str], None], globals: dict, locals: dict
-):
+def repl(read: Callable[[], str], write: Callable[[str], None]):
     while True:
-        write("\n🐍 ")
+        write("\n> ")
         try:
-            write(str(eval(read(), globals, locals)))
+            for line in (
+                subprocess.check_output(["bash", "-c", read().strip()])
+                .decode("utf-8")
+                .splitlines(keepends=True)
+            ):
+                write(line)
         except Exception:
             write(traceback.format_exc())
 
@@ -33,4 +49,4 @@ if __name__ == "__main__":
     import sys
 
     address, port = sys.argv[1:]
-    netrepl(address, int(port), globals(), locals())
+    netrepl(address, int(port))
