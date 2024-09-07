f70588e12c4916b838482a144d458488b1349062
Dan Davison <dandavison7@gmail.com>
2024-09-06 22:58:17 -0400

    netrepl

--- netrepl.py
+++ netrepl.py
@@ -2,9 +2,9 @@
 """
 1. Run ngrok on your local machine, e.g. `ngrok tcp 7777` and note the ngrok IP and port
 
-2. Add a line like this to a GitHub Actions YAML job definition:
+2. Temporarily commit the script and add a line like this to a GitHub Actions YAML job definition:
    ```
-   ./oss-cicd/internal/github/netrepl <ngrok-ip> <ngrok-port>
+   python netrepl.py <ngrok-ip> <ngrok-port>
    ```
 
 3. Run `nc -l 7777` on your local machine, and then run the GitHub workflow
@@ -35,13 +35,19 @@ def repl(read: Callable[[], str], write: Callable[[str], None]):
     while True:
         write("\n> ")
         try:
-            for line in (
-                subprocess.check_output(["bash", "-c", read().strip()])
-                .decode("utf-8")
-                .splitlines(keepends=True)
-            ):
+            input = read().strip()
+            if input == "exit":
+                break
+            proc = subprocess.run(["bash", "-c", input], capture_output=True)
+            if proc.returncode != 0:
+                write(f"exit code: {proc.returncode}\n")
+            if proc.stderr:
+                for line in proc.stderr.decode("utf-8").splitlines(keepends=True):
+                    write(f"stderr: {line}")
+            for line in proc.stdout.decode("utf-8").splitlines(keepends=True):
                 write(line)
-        except Exception:
+        except Exception as err:
+            write(f"error: {err}")
             write(traceback.format_exc())
 
 
