673d680cf634563abeb142427449878f950a997a
Dan Davison <dandavison7@gmail.com>
2025-03-31 13:46:20 -0400

    decorators

--- decorators.py
+++ decorators.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Type
 
 
@@ -11,3 +13,24 @@ class X:
 
 
 print(X)
+
+
+class Y:
+    def f(self) -> Y:
+        return self
+
+
+print(Y().f())
+
+
+def x_method(fn: Callable[[Any], None]) -> Callable[[X, Any], None]:
+    def wrapper(self: X, arg: Any) -> None:
+        return fn(arg)
+
+    return wrapper
+
+
+class X:
+    @x_method
+    def foo(arg: int) -> None:
+        pass
